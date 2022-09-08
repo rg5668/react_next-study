666659a579ce4e968af0fe8df86f9c49899e2201
Ken <rg4093@naver.com>
2022-09-08 09:10:09 +0900

    add route

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,10 +1,29 @@
 import Todo from "./components/Todo";
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetup from "./pages/AllMeetup";
+import NewAllMeetup from "./pages/NewmMeetup";
+import Favorites from "./pages/Favorites";
 
 function App() {
   return (
     <div>
       <h1>Test</h1>
       <Todo text="할일" />
+      <Switch>
+        {/* exact를 설정하면 path가 딱 맞아 떨어지는것만 해당 */}
+        <Route path={"/"} exact>
+          <AllMeetup />
+        </Route>
+
+        <Route path={"/new-meetup"}>
+          <NewAllMeetup />
+        </Route>
+
+        <Route path={"/favorites"}>
+          <Favorites />
+        </Route>
+      </Switch>
     </div>
   );
 }
