07f5c7c8925fc131afd4793bc56b80ce93743582
Ken <rg4093@naver.com>
2022-09-16 14:39:05 +0900

    2022.09.16

--- react-course/src/pages/AllMeetup.js
+++ react-course/src/pages/AllMeetup.js
@@ -2,38 +2,59 @@ import { useEffect, useState } from "react";
 import MeetupItem from "../components/meetups/MeetupItem";
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_DATA = [
-  {
-    id: "m1",
-    title: "This is a first meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "This is a second meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-];
+// const DUMMY_DATA = [
+//   {
+//     id: "m1",
+//     title: "This is a first meetup",
+//     image:
+//       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+//     address: "Meetupstreet 5, 12345 Meetup City",
+//     description:
+//       "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+//   },
+//   {
+//     id: "m2",
+//     title: "This is a second meetup",
+//     image:
+//       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+//     address: "Meetupstreet 5, 12345 Meetup City",
+//     description:
+//       "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+//   },
+// ];
 
 function AllMeetup() {
-  const [data, setData] = useState(DUMMY_DATA);
-  useEffect(() => {
-    setData(DUMMY_DATA);
-  }, []);
+  // 로딩 상태에서 시작
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadeMeetups, setLoadeMeetups] = useState([]);
+
+  fetch(
+    "https://react-getting-started-9f5df-default-rtdb.firebaseio.com/meetups.json"
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      // 데이터를 얻은 후 false
+      setIsLoading(false);
+      setLoadeMeetups(data);
+    });
+
+  useEffect(() => {}, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>...Loading</p>
+      </section>
+    );
+  }
 
   return (
     <section>
       <h1>All Meetup Page</h1>
 
-      <MeetupList meetups={data} />
+      <MeetupList meetups={loadeMeetups} />
     </section>
   );
 }
--- react-course/src/pages/NewMeetup.js
+++ react-course/src/pages/NewMeetup.js
@@ -1,7 +1,13 @@
+import { useHistory } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewAllMeetup() {
+  //useHistory는 브라우저 기록을 조작할 수 있는 메서드
+  const history = useHistory();
+
+  // Form에서 전달 받은 meetupData를 통해 데이터 저장
   function addMeetupHandler(meetupData) {
+    // fetch Promise를 반환
     fetch(
       "https://react-getting-started-9f5df-default-rtdb.firebaseio.com/meetups.json",
       {
@@ -11,7 +17,9 @@ function NewAllMeetup() {
           "Content-Type": "application/json",
         },
       }
-    );
+    ).then(() => {
+      history.replace("/");
+    });
   }
 
   return (
