5d69d45ce9b337e61cdca9409810c58ac689789d
Ken <rg4093@naver.com>
2022-10-10 16:29:38 +0900

    section8 144강 완료

---
+++ section8/pages/index.js
@@ -0,0 +1,42 @@
+import { useRef } from "react";
+
+export default function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    const reqBody = { email: enteredEmail, text: enteredFeedback };
+    fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your feedback</label>
+          <textarea rows="5" id="feedback" ref={feedbackInputRef} />
+        </div>
+        <button>Send Feedback</button>
+      </form>
+    </div>
+  );
+}
