7dd9a453ef486b6195ece22e1a8b0cd99f19e5a6
Ken <rg4093@naver.com>
2022-10-21 09:18:24 +0900

    배포 테스트

--- section10/components/home-page/hero.js
+++ section10/components/home-page/hero.js
@@ -13,7 +13,7 @@ const Hero = () => {
           layout="responsive"
         />
       </div>
-      <h1>Hi! Web Frontend Developer "Lim KunHee"</h1>
+      <h1>Hi! Web Frontend Developer Lim KunHee</h1>
       <p>
         블로그 소개 (웹 개발자) <br />
         프론트엔드 프레임워크 Next.js를 사용합니다.
--- section10/components/layout/logo.js
+++ section10/components/layout/logo.js
@@ -1,7 +1,7 @@
 import classes from "./logo.module.css";
 
 const Logo = () => {
-  return <div className={classes.logo}>KunHee' Next Blog</div>;
+  return <div className={classes.logo}>KunHee Next Blog</div>;
 };
 
 export default Logo;
