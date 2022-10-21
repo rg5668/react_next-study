3c59787063c51dbe8115b72ff52f378456dc9167
Ken <rg4093@naver.com>
2022-10-21 10:18:32 +0900

    테스트...

--- section10/components/home-page/hero.js
+++ section10/components/home-page/hero.js
@@ -16,7 +16,7 @@ const Hero = () => {
       <h1>Hi! Web Frontend Developer Lim KunHee</h1>
       <p>
         블로그 소개 (웹 개발자) <br />
-        프론트엔드 프레임워크 Next.js를 사용합니다.
+        프론트엔드 프레임워크 Next.js를 사용합니다. test
       </p>
     </section>
   );
